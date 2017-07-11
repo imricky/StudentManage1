e4a6e2bc8df23b901025b4cc66499dbbd1c31bd8
ricky <rqcao@qq.com>
2017-07-11 11:21:27 +0800

    添加项目文件。

---
+++ studentManagement1.0/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentManagement1._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Mainpage()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult he()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+
+    //学生信息控制器
+    public class StudentController : Controller {
+        public ActionResult ShowStudentInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddStudentInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    //课程控制器
+    public class CourseController : Controller
+    {
+        public ActionResult ShowCourseInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult AddCourseInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+
+
+    //成绩信息控制器
+    public class GradeController : Controller
+    {
+        public ActionResult ShowGradeInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult AddGradeInfo()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+
+
+}
---
+++ studentManagement1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="studentManagement1._0.MvcApplication" Language="C#" %>
