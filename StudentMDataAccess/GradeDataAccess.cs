f399693cd5ccb742e4cde068367f96bd21810ce6
ricky <rqcao@qq.com>
2017-07-17 09:48:36 +0800

    修复成绩查询bug

--- StudentMDataAccess/GradeDataAccess.cs
+++ StudentMDataAccess/GradeDataAccess.cs
@@ -13,7 +13,9 @@ namespace StudentMDataAccess
         public DataTable GetListAdo()
         {
             //string sql = "select stuInfo.id as 'id',stuInfo.name as 'name',sum(case courseInfo.kch when '1' then gradeInfo.ach else 0 end) as 'ach1',sum(case courseInfo.kch when '2' then gradeInfo.ach else 0 end) as 'ach2',sum(case courseInfo.kch when '3' then gradeInfo.ach else 0 end) as 'ach3',sum(case courseInfo.kch when '4' then gradeInfo.ach else 0 end) as 'ach4',sum(case courseInfo.kch when '5' then gradeInfo.ach else 0 end) as 'ach5' from gradeInfo,studentInfo,courseInfo where (gradeInfo.id = studentInfo.id) and (gradeInfo.kch = courseInfo.kch) group by stuInfo.id,stuInfo.name";
-            string sql = "select stuInfo.id as 'id',stuInfo.name as 'name',sum(case courseInfo.kch when '1' then gradeInfo.ach else 0 end) as 'ach1',sum(case courseInfo.kch when '2' then gradeInfo.ach else 0 end) as 'ach2', sum(case courseInfo.kch when '3' then gradeInfo.ach else 0 end) as 'ach3', sum(case courseInfo.kch when '4' then gradeInfo.ach else 0 end) as 'ach4',sum(gradeInfo.ach) as 'ach5' from gradeInfo, stuInfo, courseInfo  where(gradeInfo.id = stuInfo.id) and(gradeInfo.kch = courseInfo.kch)  group by stuInfo.id,stuInfo.name";
+            //string sql = "select stuInfo.id as 'id',stuInfo.name as 'name',sum(case courseInfo.kch when '1' then gradeInfo.ach else 1 end) as 'ach1',sum(case courseInfo.kch when '2' then gradeInfo.ach else 0 end) as 'ach2', sum(case courseInfo.kch when '3' then gradeInfo.ach else 0 end) as 'ach3', sum(case courseInfo.kch when '4' then gradeInfo.ach else 0 end) as 'ach4',sum(gradeInfo.ach) as 'ach5' from gradeInfo, stuInfo, courseInfo  where(gradeInfo.id = stuInfo.id) and(gradeInfo.kch = courseInfo.kch)  group by stuInfo.id,stuInfo.name";
+            string sql = "select stuInfo.id as 'id',stuInfo.name as 'name',sum(case courseInfo.kch when '0001' then gradeInfo.ach else 0 end) as 'ach1',sum(case courseInfo.kch when '0002' then gradeInfo.ach else 0 end) as 'ach2', sum(case courseInfo.kch when '0003' then gradeInfo.ach else 0 end) as 'ach3', sum(case courseInfo.kch when '0004' then gradeInfo.ach else 0 end) as 'ach4',sum(gradeInfo.ach) as 'ach5' from gradeInfo, stuInfo, courseInfo  where(gradeInfo.id = stuInfo.id) and(gradeInfo.kch = courseInfo.kch)  group by stuInfo.id,stuInfo.name";
+            //string sqltest = ""
             DataTable dt = DBUtil.GetDataTable(sql);
             return dt;
         }
@@ -62,34 +64,45 @@ namespace StudentMDataAccess
             if (grade.kch == null)
             {
                 sql = "select stuInfo.id as 'id',stuInfo.name as 'name'," +
-                    "sum(case course.kch when '1' then grade.ach else 0 end) as 'ach1'," +
-                    "sum(case course.kch when '2' then grade.ach else 0 end) as 'ach2', " +
-                    "sum(case course.kch when '3' then grade.ach else 0 end) as 'ach3', " +
-                    "sum(case course.kch when '4' then grade.ach else 0 end) as 'ach4'," +
-                    "sum(grade.ach) as 'ach5'" +
+                    "sum(case courseInfo.kch when '0001' then gradeInfo.ach else 0 end) as 'ach1'," +
+                    "sum(case courseInfo.kch when '0002' then gradeInfo.ach else 0 end) as 'ach2', " +
+                    "sum(case courseInfo.kch when '0003' then gradeInfo.ach else 0 end) as 'ach3', " +
+                    "sum(case courseInfo.kch when '0004' then gradeInfo.ach else 0 end) as 'ach4'," +
+                    "sum(gradeInfo.ach) as 'ach5'" +
                     "from gradeInfo, stuInfo, courseInfo" +
-                    "where(grade.id = student.id) and((grade.id = '" + grade.id + "')or('" + grade.id + "' = null)) and(grade.kch = course.kch)and" +
-                    "(grade.id in" +
-                    "(select id from grade" +
+                    "where(gradeInfo.id = stuInfo.id) and((gradeInfo.id = '" + grade.id + "')or('" + grade.id + "' = null)) and(gradeInfo.kch = courseInfo.kch) and" +
+                    "(gradeInfo.id in" +
+                    "(select id from gradeInfo" +
                     "where ((ach >= " + grade.achlow + ")and(ach <= " + grade.achhigh + "))))" +
-                    "group by student.id,student.name";
+                    "group by stuInfo.id,stuInfo.name";
             }
             else
             {
-                sql = "select student.id as 'id',student.name as 'name'," +
-                    "sum(case course.kch when '1' then grade.ach else 0 end) as 'ach1'," +
-                    "sum(case course.kch when '2' then grade.ach else 0 end) as 'ach2', " +
-                    "sum(case course.kch when '3' then grade.ach else 0 end) as 'ach3', " +
-                    "sum(case course.kch when '4' then grade.ach else 0 end) as 'ach4'," +
-                    "sum(grade.ach) as 'ach5'" +
-                    "from grade, student, course" +
-                    "where(grade.id = student.id) and((grade.id = '" + grade.id + "')or('" + grade.id + "' = null)) and(grade.kch = course.kch)and" +
-                    "(grade.id in" +
-                    "(select id from grade" +
-                    "where ((ach >= " + grade.achlow + ")and(ach <= " + grade.achhigh + ")and (grade.kch = '" + grade.kch + "'))))" +
-                    "group by student.id,student.name";
-            }
-            // string sql = "select * from student where(( id = '" + stu.id + "'or '" + stu.id + "' = '') and (name = '" + stu.name + "'or '" + stu.name + "' = '') and (sex = '" + stu.sex + "'or '" + stu.sex + "' =  0) and (bir = '" + stu.bir + "'or '" + stu.bir + "' = '') and (cla ='" + stu.cla + "'or '" + stu.cla + "' = '') and (adr ='" + stu.adr + "'or '" + stu.adr + "' = ''))";
+                //sql = "select student.id as 'id',student.name as 'name'," +
+                //    "sum(case course.kch when '1' then grade.ach else 0 end) as 'ach1'," +
+                //    "sum(case course.kch when '2' then grade.ach else 0 end) as 'ach2', " +
+                //    "sum(case course.kch when '3' then grade.ach else 0 end) as 'ach3', " +
+                //    "sum(case course.kch when '4' then grade.ach else 0 end) as 'ach4'," +
+                //    "sum(grade.ach) as 'ach5'" +
+                //    "from grade, student, course" +
+                //    "where(grade.id = student.id) and((grade.id = '" + grade.id + "')or('" + grade.id + "' = null)) and(grade.kch = course.kch)and" +
+                //    "(grade.id in" +
+                //    "(select id from grade" +
+                //    "where ((ach >= " + grade.achlow + ")and(ach <= " + grade.achhigh + ")and (grade.kch = '" + grade.kch + "'))))" +
+                //    "group by student.id,student.name";
+                sql = "select stuInfo.id as 'id',stuInfo.name as 'name'," +
+                    "sum(case courseInfo.kch when '0001' then gradeInfo.ach else 0 end) as 'ach1'," +
+                    "sum(case courseInfo.kch when '0002' then gradeInfo.ach else 0 end) as 'ach2', " +
+                    "sum(case courseInfo.kch when '0003' then gradeInfo.ach else 0 end) as 'ach3', " +
+                    "sum(case courseInfo.kch when '0004' then gradeInfo.ach else 0 end) as 'ach4'," +
+                    "sum(gradeInfo.ach) as 'ach5'" +
+                    "from gradeInfo, stuInfo, courseInfo" +
+                    "where(gradeInfo.id = stuInfo.id) and((gradeInfo.id = '" + grade.id + "')or('" + grade.id + "' = null)) and(gradeInfo.kch = courseInfo.kch)and" +
+                    "(gradeInfo.id in" +
+                    "(select id from gradeInfo" +
+                    "where ((ach >= " + grade.achlow + ")and(ach <= " + grade.achhigh + ") and (gradeInfo.kch = '" + grade.kch + "'))))" +
+                    "group by stuInfo.id,stuInfo.name";
+            }          
             DataTable dt = DBUtil.GetDataTable(sql);
             return dt;
         }
@@ -104,7 +117,6 @@ namespace StudentMDataAccess
         public string id { get; set; }
         public int ach { get; set; }
         public string remarks { get; set; }
-
         public int achhigh { get; set; }
 
         public int achlow { get; set; }
